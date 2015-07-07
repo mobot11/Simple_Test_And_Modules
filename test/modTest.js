de892520a4c0bfab92382cb18238abc5dc8d33ea
Dylan T. Moberg <dylanmoberg@msn.com>
2015-07-07 12:08:01 -0700

    added command line functionality

--- test/modTest.js
+++ test/modTest.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 var greet = require("../greet.js").greet;
 
 describe('greet', function() {
-	it('should return hello + "name"', function () {
-		expect(greet('Dylan')).to.eql("Hello Dylan");
+	it('should return hello + undefined or "name"', function () {
+		expect(greet('Dylan')).to.eql("Hello Dylan" || undefined);
 	});
 });
