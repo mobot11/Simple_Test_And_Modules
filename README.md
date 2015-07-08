618c4cc657df9306bebdd5c3e5b87985671d5d8c
Dylan T. Moberg <dylanmoberg@msn.com>
2015-07-08 11:13:51 -0700

    added depenencies and gulp tests, added jshint and jshint preferences

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my first CodeFellows Javascript Development Accelerator project.
--- greet.js
+++ greet.js
@@ -1,11 +1,14 @@
 function greet(name) {
-	if (process.argv[2] != undefined) {
-		return "Hello " + process.argv[2];
+	if (process.argv[3] !== undefined) {
+		return "Hello " + process.argv[3];
 	}
 	else return "Hello " + name;
 }
 
 exports.greet = greet;
+
+
+
 
 
 
